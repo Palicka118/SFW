c1b2b3685c020e6d970463fcb5c1282663eb4121
Palicka118 <palicka.pavel19@gmail.com>
2021-06-11 07:55:49 +0200

    Final commit before cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,22 @@
 package com.company;
 
-
 import java.util.Scanner;
 import java.lang.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.lang.String;
+import java.lang.Integer;
 
 
 public class Main {
-    //Zpráva o jakékoliv chybě
-    static final String errorMessage = "Zadali jste \u0161patn\u00E9 rodn\u00E9 \u010D\u00EDslo";
+
+    //static final String errorMessage = "Zadali jste \u0161patn\u00E9 rodn\u00E9 \u010D\u00EDslo";
     public static void main(String[] args) {
 
         System.out.println("Zadejte prosím Vaše rodné číslo: ");
         Scanner sc = new Scanner(System.in);
-        String BirthNumber=sc.next();
-        //OvereniRodneCislo(BirthNumber);
-        if(InputVerification(BirthNumber)) DateBirthVerification(BirthNumber);
+        String birthNumber=sc.next();
+        if(InputVerification(birthNumber)) DateBirthVerification(birthNumber);
         else{
             System.out.println("Zadali jste špatné rodné číslo");
         }
@@ -25,35 +24,101 @@ public class Main {
     }
 
     //kontrola, jestli má vstup formát rodného čísla
-    public static boolean InputVerification(String BirthNumber){
+    public static boolean InputVerification(String birthNumber){
         String Pattern1 ="[0-9]{6}/?[0-9]{3,4}";
         Pattern pt = Pattern.compile(Pattern1);
-        Matcher mt = pt.matcher(BirthNumber);
+        Matcher mt = pt.matcher(birthNumber);
         return mt.matches();
     }
 
     //Kontrola, zda má vstup všechny potřebné požadavky
-    public static String DateBirthVerification(String BirthNumber){
-        char[] dateBirth = BirthNumber.toCharArray();
-        BirthNumber = BirthNumber.replaceAll("[^\\d]", "");
-        int x = Integer.parseInt(BirthNumber);
-        int maxDay1 = 3, maxDay2 = 1;
-        if(/*(x %11 ==0) && */(dateBirth[4]<3 && dateBirth[5]>1)||(dateBirth[4]==3 && dateBirth[5]<2)/*&&(dateBirth[2]<=3&&dateBirth[3]<=9)*/){
-            System.out.println("true");
-            //DateBirthConvert();
-            return Character.toString(dateBirth[2]);
+    public static void DateBirthVerification(String birthNumber){
+        String[] arr = birthNumber.split("/");
+        String arr1=arr[0] + arr[1];
+        char[] dateBirth2 = arr1.toCharArray();
+        int[] dateBirth = new int[10];
+        for(int i = 0; i < dateBirth2.length; ++i) {
+            dateBirth[i] = (dateBirth2)[i] - 48;
         }
+        String rokNarozeni= dateBirth[0] +Integer.toString(dateBirth[1]);
+        int rokNarozeniInt = Integer.parseInt(rokNarozeni);
+        rokNarozeniInt=GetBirthYear(rokNarozeniInt);
 
-        else
-        {
-            System.out.println(errorMessage);
-            return errorMessage;
+        String birthDay = dateBirth[4] +Integer.toString(dateBirth[5]);
+        int birthDayInt=Integer.parseInt(birthDay);
+        //GetBirthDay(birthDayInt);
+
+        String mesic = (dateBirth[2]) + Integer.toString(dateBirth[3]);
+        int mesicInt = Integer.parseInt(mesic);
+
+        //System.out.println(birthDay);
+        if((DayIsValid(birthDayInt))&&(MonthIsValid(dateBirth[2]))&&(DivideVer(dateBirth, birthNumber))){
+            System.out.println("true");
+            DateBirthConvert(birthDayInt, mesicInt, rokNarozeniInt);
         }
     }
 
     //Konverze vstupu do formát data narození
-    public static String DateBirthConvert(){
+    public static void DateBirthConvert(int birthDay, int birthMonth, int birthYear){
+        System.out.println("Vase datum narození je:"+birthDay+". "+birthMonth+". "+birthYear);
+    }
 
-        return "0";
+    //Kontrola měsíce
+    public static boolean MonthIsValid(int Month){
+        return GetGender(Month) < 12;
+    }
+
+    /*public static int GetBirthMonth(){
+
+        return 0;
+    }*/
+
+    public static int GetGender(int Month){
+        if(Month>5){
+            int tmp=Month;
+            tmp=tmp-5;
+            Month=(char)tmp;
+            return Month;
+        }
+        else return Month;
+
+    }
+
+
+
+    public static boolean DayIsValid(int BirthDay){
+        return BirthDay < 32;
+    }
+
+
+    /*public static int GetBirthDay(int BirthDay){
+
+        return BirthDay;
+    }*/
+
+
+    public static int GetBirthYear(int rokNarozeni){
+        if(rokNarozeni<22){
+            rokNarozeni+=2000;
+        }
+        else{
+            rokNarozeni+=1900;
+        }
+        return rokNarozeni;
+    }
+
+    //Kontrola delitelnosti
+    public static boolean DivideVer(int[] dateBirth, String dateBirth2){
+        int lichaCisla = dateBirth[0]+dateBirth[2]+dateBirth[4]+dateBirth[6]+dateBirth[8];
+        int sudaCisla = dateBirth[1] + dateBirth[3] + dateBirth[5] + dateBirth[7]+dateBirth[9];
+        if(dateBirth2.length()==9){
+            return true;
+        }
+        else if((lichaCisla-sudaCisla)==0||(lichaCisla-sudaCisla)%11==0){
+           return true;
+        }
+        else{
+        System.out.println("špatná kontrolní číslice");
+            return false;}
     }
 }
